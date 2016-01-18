dff46d3d1aa0505dd7aedd8d2a23a3752d289925
Ariel Liu <aliu@lyft.com>
2016-01-18 14:00:17 -0800

    Initial Commit

---
+++ codepath_wk1_tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codepath_wk1_tips
+//
+//  Created by Ariel Liu on 1/18/16.
+//  Copyright © 2016 Ariel Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
