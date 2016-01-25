e81a059d96b1e663ede9f9158d0882c20dd0f032
Ariel Liu <aliu@lyft.com>
2016-01-24 18:01:21 -0800

    tip calculator pre-assignment

--- codepath_wk1_tips/ViewController.swift
+++ codepath_wk1_tips/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentageControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        tipPercentageControl.selectedSegmentIndex = 0
+        billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +30,23 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipPercentageControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
